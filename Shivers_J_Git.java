d7910fc3f669ecf074a42360f803691549fc0fee
unknown <joshuashivers@CTEC.LOCAL>
2020-01-27 10:33:32 -0600

    first commit

---
+++ Shivers_J_Git.java
@@ -0,0 +1,19 @@
+//Joshua Shivers
+//Git Project
+
+package shivers_j_git;
+
+/**
+ *
+ * @author joshuashivers
+ */
+public class Shivers_J_Git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello World");
+    }
+    
+}
